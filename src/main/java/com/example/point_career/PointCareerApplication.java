0b39a92827690cca6fe457cddbfecd6a890ecedc
sunkong25 <kcindy325k@kyonggi.ac.kr>
2025-05-28 11:10:16 +0900

    init: 프로젝트 초기 설정
    - 스프링부트 환경설정
    - git commit message template 작성

---
+++ src/main/java/com/example/point_career/PointCareerApplication.java
@@ -0,0 +1,13 @@
+package com.example.point_career;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointCareerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointCareerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/point_career/PointCareerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.point_career;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointCareerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
