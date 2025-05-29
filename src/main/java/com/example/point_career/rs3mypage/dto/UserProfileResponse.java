6d0d62baf6244b86cca4c420d3f246ce98e980a4
CommonLion <tndud4327@naver.com>
2025-05-29 16:05:44 +0900

    chore: 자료 구성 형태 변경
    - UserProfileResponse.java의 상속된 클래스 분류

--- src/main/java/com/example/point_career/rs3mypage/dto/UserProfileResponse.java
+++ src/main/java/com/example/point_career/rs3mypage/dto/UserProfileResponse.java
@@ -1,7 +1,6 @@
 package com.example.point_career.rs3mypage.dto;
 
 import lombok.*;
-import java.util.List;
 
 @Getter @Setter
 public class UserProfileResponse {
@@ -9,36 +8,3 @@ public class UserProfileResponse {
     private Bookmarks bookmarks;
 }
 
-@Getter @Setter
-public class UserProfile {
-    private String user_name;
-    private String user_major;
-    private String user_grade;
-    private List<String> user_favorites;
-    private List<String> user_industries;
-}
-
-@Getter @Setter
-public class Bookmarks {
-    private List<PointBookmark> points;
-    private List<RecruitBookmark> recruits;
-}
-
-@Getter @Setter
-public class PointBookmark {
-    private Long point_id;
-    private String point_title;
-    private int point_price;
-    private String point_deadline;
-    private int point_duration;
-    private boolean is_point_online;
-    private String point_link_url;
-}
-
-@Getter @Setter
-public class RecruitBookmark {
-    private Long recruit_id;
-    private String recruit_title;
-    private String recruit_deadline;
-    private List<String> tag;
-}
