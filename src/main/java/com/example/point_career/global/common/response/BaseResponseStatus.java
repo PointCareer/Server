d58eeed27bf6ed17f0aa540fd1e1e74213219edb
sunkon25 <kcindy325k@kyonggi.ac.kr>
2025-05-29 16:18:13 +0900

    feat: BaseResponse 생성
    - BaseResponseStatus 생성
    - BaseResponse 생성
    - BaseException 생성
    - GlobalExceptionHandler 생성

---
+++ src/main/java/com/example/point_career/global/common/response/BaseResponseStatus.java
@@ -0,0 +1,22 @@
+package com.example.point_career.global.common.response;
+
+import lombok.Getter;
+
+@Getter
+public enum BaseResponseStatus {
+	//TODO: 상황에 맞는 메시지 추가해서 사
+
+	SUCCESS("success", 200, "요청에 성공했습니다."),
+	FAIL("fail", 400, "요청에 실패했습니다."),
+	VALIDATION_ERROR("fail", 400, "유효성 검사에 실패했습니다.");
+
+	private final String status;
+	private final int code;
+	private final String message;
+
+	BaseResponseStatus(String status, int code, String message) {
+		this.status = status;
+		this.code = code;
+		this.message = message;
+	}
+}
