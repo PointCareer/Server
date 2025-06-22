cfaadeec94fd60defcd8d35f130d8986cb102141
sunkon25 <kcindy325k@kyonggi.ac.kr>
2025-06-23 00:39:04 +0900

    fix: CORS 수정
    - 프론트엔드의 정확한 경로 설정

--- src/main/java/com/example/point_career/global/common/config/WebConfig.java
+++ src/main/java/com/example/point_career/global/common/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.example.point_career.global.common.config;
 
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -14,7 +15,9 @@ public class WebConfig {
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration config = new CorsConfiguration();
 
-		config.addAllowedOriginPattern("*"); // 모든 도메인 허용 (보안상 주의)
+		//config.addAllowedOrigin("http://10.0.2.2:*");
+		//config.addAllowedOriginPattern("*"); // 모든 도메인 허용 (보안상 주의)
+		config.setAllowedOriginPatterns(List.of("http://10.0.2.2:*"));
 		config.addAllowedMethod("*");        // GET, POST, PUT, DELETE 등 모두 허용
 		config.addAllowedHeader("*");        // 모든 헤더 허용
 		config.setAllowCredentials(true);    // 쿠키 허용 (필요한 경우)
